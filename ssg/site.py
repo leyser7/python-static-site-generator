22dad1effe2566b62d26c142199178b550b44b78
Cristian Alzate <ccalzate@bancolombia.com.co>
2021-06-10 23:20:16 -0500

    se agrega el modulo 2

--- ssg/site.py
+++ ssg/site.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
-        self.source = Path(source)
+    def __init__(self, source, dest, parsers=None):
+        self.parsers = parsers or []
+        self.source = Path(source)  
         self.dest = Path(dest) 
 
     def create_dir(self, path):
@@ -14,3 +15,17 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+    
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser 
+    
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
